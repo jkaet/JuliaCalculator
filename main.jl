df50cb8acc9e017f9184371eb396a2969a4ad14b
JaQuan Taylor <jkaet@protonmail.com>
2020-04-06 02:01:34 -0400

    finished basic blink calc

--- main.jl
+++ main.jl
@@ -1,8 +1,15 @@
+module JuliaCalc
+export mycalc
+
 using Blink
 
-
+function mycalc()
 w = Window(async=false)
 f = open("index.html") do file
     read(file,String)
 end
 body!(w,f)
+end
+
+mycalc()
+end
